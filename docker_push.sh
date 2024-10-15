f49ac15642566a3611cde37a892b6a1f173a34c7
nicksonthc <tan.hoo.chuan@pentamaster.com.my>
2024-10-15 11:34:31 +0800

    feat : remove credential

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker login -u deploy-token -p dJZ9XiewfZOXDfiR/voWWKD7ObKutV5R4F4/ZJTwZo+ACRCEoAxl pentapss.azurecr.io
-
 docker tag pcs-rasp:latest pentapss.azurecr.io/pcs-rasp:latest
 
 docker push pentapss.azurecr.io/pcs-rasp:latest
